d726349cde6a0af2c30036f31ad120f3419fb417
jayanth0075 <jayanth0075@users.noreply.github.com>
2026-03-01 02:15:38 +0530

    feat: create User, Skill and ApiResponse DTOs

---
+++ backend/src/main/java/com/fedf/dto/SkillDTO.java
@@ -0,0 +1,16 @@
+package com.fedf.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SkillDTO {
+    private String name;
+    private Integer level;
+    private String category;
+}
